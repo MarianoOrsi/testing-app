1f00b719c5d24a251ad85cef06aa78ea61cc03f3
Mariano Orsi <maariano.orsi@gmail.com>
2020-05-19 02:05:35 -0300

    port parameter

--- src/config/testing.js
+++ src/config/testing.js
@@ -2,5 +2,6 @@ export const config = {
   secrets: {
     jwt: 'learneverything'
   },
-  dbUrl: 'mongodb://localhost:27017/api-design-test'
+  dbUrl:
+  'mongodb+srv://morsi:1993Bocaboca@cluster0-bvka4.azure.mongodb.net/spazzio-db-test?retryWrites=true&w=majority'
 }
